ae2f920ca51be98b3baa7a44e2bd478e9cd38f9f
The Combed Thunderclap <clappedthunder@gmail.com>
2019-11-15 14:52:27 +0000

    Add model versioning and add bootstrap js for UI improvement on serving webservice

--- webservices/serve_iris_predictions/main.py
+++ webservices/serve_iris_predictions/main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from google.cloud import storage
 from sklearn import datasets
 from joblib import load
@@ -14,7 +14,8 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     if not os.path.exists(destination_file_name):
-        return "Please download model: /download"
+        message = "Please download model first"
+        return render_template('message.html', message=message)
     else:
         global model
 
@@ -22,8 +23,9 @@ def home():
         model = load(destination_file_name)
 
         print("Making predictions..")
-        results = model.predict(X)
-        return str(results)
+        predictions = f"{model.predict(X)}"
+        real_targets = f"{y}"
+        return render_template('home.html', predictions=predictions, real_targets=real_targets)
 
 @app.route('/download')
 def download():
@@ -34,12 +36,29 @@ def download():
         print("Removing old model..")
         os.remove(destination_file_name)
 
-    print("Downloading model..")
-    download_blob(bucket_name, source_blob_name, destination_file_name)
+    print("Finding model to download..")
+    blob_names = list_blobs(bucket_name, bucket_prefix)
 
-    print("Ready to serve.")
+    model_to_download = ""
+    for blob in blob_names:
+        if model_version == "latest":
+            if blob.name.find(".latest", -8) > 0:
+                model_to_download = blob.name
+        else:
+            if blob.name.find(model_version) > 0:
+                model_to_download = blob.name
 
-    return "model downloaded, ready to serve"
+    if model_to_download == "":
+        message = f"Could not find model '{model_latest}' in bucket {bucket_name}"
+        return render_template('message.html', message=message)
+    else:
+        print("Downloading model..")
+        download_blob(bucket_name, model_to_download, destination_file_name)
+
+        print("Ready to serve.")
+
+        message = f"Downloaded model {model_to_download}, ready to serve"
+        return render_template('message.html', message=message)
 
 def download_blob(bucket_name, source_blob_name, destination_file_name):
     """Downloads a blob from the bucket."""
@@ -53,6 +72,15 @@ def download_blob(bucket_name, source_blob_name, destination_file_name):
         source_blob_name,
         destination_file_name))
 
+def list_blobs(bucket_name, prefix):
+    """Lists all the blobs in the bucket."""
+    storage_client = storage.Client()
+
+    # Note: Client.list_blobs requires at least package version 1.17.0.
+    blobs = storage_client.list_blobs(bucket_name, prefix=prefix)
+
+    return blobs
+
 @app.errorhandler(500)
 def server_error(e):
     print('An internal error occurred')
@@ -61,10 +89,13 @@ def server_error(e):
 print("Preparing..")
 project_id = os.getenv('PROJECT')
 bucket_name = os.getenv('ML_MODELS_BUCKET')
-source_blob_name = os.getenv('MODEL')
-checkpoint_dir = '/tmp/checkpoint/'
-destination_file_name = checkpoint_dir + source_blob_name
+bucket_prefix = os.getenv('BUCKET_PREFIX')
+model_version = os.getenv('MODEL_VERSION')
+model_name = os.getenv('MODEL_NAME')
+checkpoint_dir = '/tmp/checkpoint'
+destination_file_name = f"{checkpoint_dir}/{model_name}"
 
 print("Downloading iris dataset..")
 iris = datasets.load_iris()
 X, y = iris.data, iris.target
+print("Ready")
