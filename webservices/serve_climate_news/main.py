f3e5086ab00fc4efd82eb73dd766b69263624390
The Combed Thunderclap <clappedthunder@gmail.com>
2019-11-16 14:01:32 +0000

    Simplify how the model is included by letting cloud build add it for app engine deploy instead of at runtime (clnn)

--- webservices/serve_climate_news/main.py
+++ webservices/serve_climate_news/main.py
@@ -2,7 +2,6 @@ from flask import Flask
 from google.cloud import storage
 import gpt_2_simple as gpt2
 
-import shutil
 import os
 from google.cloud.exceptions import NotFound, Conflict
 
@@ -11,40 +10,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    results = gpt2.generate(sess)
-    return results
-
-def download_blob(bucket_name, source_blob_name, destination_file_name):
-    """Downloads a blob from the bucket."""
-    storage_client = storage.Client()
-    bucket = storage_client.get_bucket(bucket_name)
-    blob = bucket.blob(source_blob_name)
-
-    blob.download_to_filename(destination_file_name)
-
-    print('Blob {} downloaded to {}.'.format(
-        source_blob_name,
-        destination_file_name))
+    article = gpt2.generate(sess)
+    return render_template('home.html', article=article)
 
 @app.errorhandler(500)
 def server_error(e):
     print('An internal error occurred')
     return 'An internal error occurred.', 500
 
-print("Preparing..")
-project_id = os.getenv('PROJECT')
-bucket_name = os.getenv('ML_MODELS_BUCKET')
-source_blob_name = os.getenv('MODEL_ARCHIVE')
-destination_file_name = 'checkpoint/' + source_blob_name
-
-print("Creating checkpoint directory..")
-if not os.path.exists('checkpoint'):
-    os.makedirs('checkpoint')
-
-print("Downloading model..")
-download_blob(bucket_name, source_blob_name, destination_file_name)
-
-print("Load gpt2 session..")
+print("Loading gpt2 session..")
 sess = gpt2.start_tf_sess()
 gpt2.load_gpt2(sess)
 
