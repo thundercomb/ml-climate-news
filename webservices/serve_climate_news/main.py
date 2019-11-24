7226bcaa4538a8ce890f25b956077105e0373892
The Combed Thunderclap <clappedthunder@gmail.com>
2019-11-24 17:00:40 +0000

    Order articles, make paragraphs, and add About page.

--- webservices/serve_climate_news/main.py
+++ webservices/serve_climate_news/main.py
@@ -1,9 +1,24 @@
 from flask import Flask, render_template
 from google.cloud import storage
+from nltk.tokenize import sent_tokenize
 
 import os
 import datetime
 from google.cloud.exceptions import NotFound, Conflict
+
+def group_sentences(sentence_list, size):
+    for i in range(0, len(sentence_list), size):
+        yield sentence_list[i:i+size]
+
+def create_paragraphs(article_text,paragraph_size):
+    sentence_list = sent_tokenize(article_text)
+    sentences_groups_list = group_sentences(sentence_list,4)
+    new_article = ""
+    for group in sentences_groups_list:
+        paragraph = " ".join(group)
+        new_article = f"{new_article}{paragraph}<BR/><BR/>"
+
+    return new_article
 
 def list_blobs(bucket_name, prefix):
     """Lists all the blobs in the bucket."""
@@ -12,12 +27,37 @@ def list_blobs(bucket_name, prefix):
 
     return blobs
 
-app = Flask(__name__)
+def get_articles(bucket_name, prefix):
+    blobs = list_blobs(bucket_name, prefix)
+    articles = []
+
+    for blob in blobs:
+        blob_datetime_str = f"{blob.name.split('_')[1]}_{blob.name.split('_')[2].split('.')[0]}"
+        dt_obj = datetime.datetime.strptime(blob_datetime_str,'%Y%m%d_%H%M%S')
+        article_datetime_str = datetime.datetime.strftime(dt_obj,"News at %H:%M on %A, %d %B %Y")
+
+        article_text = blob.download_as_string().decode('utf-8')
+        article_text_trim = article_text[0:article_text.rfind('.')+1]
+
+        article_text_with_paragraphs = create_paragraphs(article_text_trim,6)
+        article_doc = f"<b><i>{article_datetime_str}</i></b><BR/><BR/>{article_text_with_paragraphs}"
+        articles.append(article_doc)
+
+    articles = articles[::-1]
+
+    return articles
+
+app = Flask(__name__, static_url_path='/static')
 
 @app.route('/')
 def home():
+    articles = get_articles(ml_articles_bucket, 'news')
     article = "<BR/><BR/>".join(articles)
     return render_template('home.html', article=article)
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 @app.errorhandler(500)
 def server_error(e):
@@ -25,18 +65,8 @@ def server_error(e):
     return 'An internal error occurred.', 500
 
 storage_client = storage.Client()
-articles = []
 
 ml_articles_bucket = os.getenv('ML_ARTICLES_BUCKET')
-blobs = list_blobs(ml_articles_bucket,'news')
-for blob in blobs:
-    blob_datetime_str = f"{blob.name.split('_')[1]}_{blob.name.split('_')[2].split('.')[0]}"
-    dt_obj = datetime.datetime.strptime(blob_datetime_str,'%Y%m%d_%H%M%S')
-    article_datetime_str = datetime.datetime.strftime(dt_obj,"News at %H:%M on %A, %d %B %Y")
 
-    article_text = blob.download_as_string().decode('utf-8')
-    article_text_trim = article_text[0:article_text.rfind(.)]
-    article_doc = f"<b>{article_datetime_str}</b><BR/><BR/>{article_text_trim}"
-    articles.append(article_doc)
 
 print("Ready to serve.")
